fca8a8874d5375d4c18692a98e0130d93d208838
Ntonsite Mwamlima <nmwamlima@accessbank.internal>
2019-09-12 09:29:26 +0300

    initial commit

---
+++ app/model/db.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var mysql = require('mysql');
+
+//local mysql db connection
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'mydb'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+});
+
+module.exports = connection;
